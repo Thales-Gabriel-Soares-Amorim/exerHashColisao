f52674abb19d5c5ea54a1b76d862ed141c92e82e
Thales-Gabriel-Soares-Amorim <109538285+Thales-Gabriel-Soares-Amorim@users.noreply.github.com>
2023-04-23 22:25:25 +0000

    padrão

--- hashColisao/EstruturaHashTable.java
+++ hashColisao/EstruturaHashTable.java
@@ -2,31 +2,51 @@ package hashColisao;
 
 public class EstruturaHashTable implements EstruturaDeDados{
     private Integer tabela[];
-    //TODO adicionar elementos necessários para implementar o porão
+    
 
     public EstruturaHashTable() {
         //esse número pode ser alterado
         tabela = new Integer[1000];
     }
 
+    public int EscolherIndice (int chave){
+        return chave % 1000;
+    }
+
+    public int Colisao (int chave){ //caso ocorra colisão, novo lugar
+        int posicaoPorao = chave % 100; //lugar no porão
+
+        return (posicaoPorao +1000); // para fiacar no porão
+    }
     @Override
     public boolean insert(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, o elemento NÃO é inserido e retorna false. Caso contrário, o elemento é inserido na posição calculada e retorna true.
-        return false;
-        
-    }
+    
+        // numero que quero inserir
+           int numero = EscolherIndice(chave);
+           //ifs verificam
+           if (tabela[numero] != null){ 
+            return false;
+           }
+            tabela[numero] = chave; //inserção
+            return true;
+           }
 
     @Override
     public boolean delete(int chave) {
-        // TODO quando inserir, se a posição estiver ocupada, torna o elemento da posição como null e retorna true. Caso contrário, retorna false.
-        return false;
+        int numero = EscolherIndice(chave);
+        if (tabela[numero] != null){
+            tabela[numero] = null; //o numero foise
+            return true;
+
+        }
+        return false; // não havia numero para apagar
         
     }
-
     @Override
     public int search(int chave) {
-        // TODO se o elemento estiver presente retorna a sua posição. Caso contrário, retorna -1.
-        return -1;
+        int numero = EscolherIndice(chave);
+        if (tabela[numero] != null &&  tabela[numero] == chave){
+            return numero; //encontrou
+        } return -1;
     }
-
 }
